132657e2586b1d2c18e5ecbc50ffcf589d031880
Denis Zhbankov <denis.zhbankov@gmail.com>
2022-03-19 19:55:23 +0300

    * master: 3sum

--- playground/playground.test.ts
+++ playground/playground.test.ts
@@ -1,22 +1,9 @@
 import assert from 'assert'
 
-function getMaxsum(a: number[], k: number): number {
-  let sum = 0
-  let maxsum = 0
-  for (let i = 0, l = a.length; i < l; i++) {
-    sum += a[i]
-    sum -= a[i - k] ?? 0
-    maxsum = Math.max(maxsum, sum)
-  }
-  return maxsum
+function foo(): number {
+  return 42
 }
 
 describe('', () => {
-  test('', () => {
-    assert(getMaxsum([2, 1, 5, 1, 3, 2], 3) === 9)
-  })
-
-  test('', () => {
-    assert(getMaxsum([2, 3, 4, 1, 5], 2) === 7)
-  })
+  test('', () => assert(foo() === 42))
 })
